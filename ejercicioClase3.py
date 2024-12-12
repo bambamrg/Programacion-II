590f3b4cf396e287e550970ca75074e87f1daecc
bambamrg <ivan_gabriel_aranda@hotmail.com>
2024-12-12 17:54:18 -0300

    commit

---
+++ ejercicioClase3.py
@@ -0,0 +1,14 @@
+#Crear un diccionario donde las claves sean los nombres de los departamentos de una empresa, y los valores sean 
+#listas de diccionarios representando a los empleados de cada departamento, con atributos como nombre, cargo, y 
+#salario.
+
+empresa = {"Recursos Humanos": [{"nombre":"Juan Perez", "cargo":"Jefe RRHH", "salario":500000}],
+           "Marketing": [{"nombre": "Pablo Marmol", "cargo":"Jefe de Marketing", "salario": 450000}],
+           "Contabilidad": [{"nombre":"Cosme Fulanito", "cargo":"Jefe de Contabilidad", "salario": 400000}]}
+
+empresa1 = {
+    "Recursos Humanos": [{"nombre":"Juan","cargo":"Secretario","salario":23000},
+                         {"nombre":"Carmen","cargo":"Recepcion","salario":12000}],
+    "Marketing": []
+}
+
